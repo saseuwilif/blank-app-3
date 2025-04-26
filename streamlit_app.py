2a6188f6ef1b1cf5999a948606e3c705ef0a7644
saseuwilif <saseuwilif@gmail.com>
2025-04-26 08:09:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("🎈 Welcome to Substance Calculator")
-st.write("Aplikasi berbasis web untuk membantu para analis kimia dalam penelitian"
+st.write("Aplikasi berbasis web untuk membantu para analis kimia dalam penelitian")
